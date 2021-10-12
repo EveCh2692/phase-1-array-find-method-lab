895b940b520e812e97adb17a920f5dd91d69d286
Evelyn <evelyn.chavez92@yahoo.com>
2021-10-12 15:22:40 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+    
+  ]
+ const superbowlWin = record.find(year => year === 2017)
